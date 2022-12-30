979fb1b18558e9e8ae739e81e83ff25cc0e28d97
Andrés Ibarra Guerrero <andresmcio@Andress-MacBook-Pro.local>
2022-12-30 13:46:15 -0500

    Actividad finalizada

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -2,3 +2,38 @@ console.log("hello world");
 
 
 console.log("laater");
+
+let alumnName = "Andrés Ibarra Guerrero";
+let ocupation = "Ingeniero preventa";
+let position = "Desarrollador";
+let message = `Hola, me llamo ${alumnName} y me dedico a ${ocupation}. Estoy cursando este Máster porque me gustaría trabajar como ${position}`;
+
+console.log(message);
+
+// Ejecicio 4
+
+function divisibleX7() {
+    for (let i = 0; i < 100; i++) {
+        if (i % 7 === 0) {
+            console.log(i);
+        }
+    }
+}
+divisibleX7();
+
+// Ejercicio 6
+
+let formatter = {
+    prefix: "Hello",
+    append: (str) => {
+        console.log(`${formatter.prefix} ${str}`);
+    }
+}
+
+formatter.append("World");
+
+formatter.toLowerString = (str) => {
+    console.log(str.toLowerCase());
+};
+
+formatter.toLowerString("HELLO WORLD");
